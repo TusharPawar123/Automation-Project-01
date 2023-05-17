d45bdd0c75da783bde7ae4486a26b00b62969d22
TusharPawar123 <tusharpawar82018@gmail.com>
2023-05-17 10:44:25 +0530

    my first commit

---
+++ addition/AddTwoNumber.java
@@ -0,0 +1,16 @@
+package addition;
+
+public class AddTwoNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 5;
+int b = 4;
+
+  int sum =a+b;
+  
+  System.out.println(sum);
+  
+	}
+
+}
