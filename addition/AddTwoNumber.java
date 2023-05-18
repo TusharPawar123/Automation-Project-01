eaabdcba97b16ecb93fa87f973f5a8de0557ec12
TusharPawar123 <tusharpawar82018@gmail.com>
2023-05-18 09:26:34 +0530

    modify that code

--- addition/AddTwoNumber.java
+++ addition/AddTwoNumber.java
@@ -4,6 +4,8 @@ public class AddTwoNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// this comment for practice
+		//lets do it
 int a = 5;
 int b = 4;
 
